fd31d94d0adb51174e07787ca86ad176fd380e97
ligia <ligia@DESKTOP-33DDFM8>
2022-10-20 23:01:46 -0300

    commit cubo

--- cubo/src/cubo/Cubo.java
+++ cubo/src/cubo/Cubo.java
@@ -2,6 +2,52 @@ package cubo;
 
 public class Cubo {
 
+	private double a;
+	private double m;
+	private int tinta;
 	
+	public Cubo(double a, double m, int tinta) {
+		super();
+		this.a = a;
+		this.m = m;
+		this.tinta = tinta;
+	}
 
+	//metodos
+
+
+	public double getA() {
+		return a;
+	}
+
+
+
+	public void setA(double a) {
+		this.a = a;
+	}
+
+
+
+	public double getM() {
+		return m;
+	}
+
+
+
+	public void setM(double m) {
+		this.m = m;
+	}
+
+
+
+	public int getTinta() {
+		return tinta;
+	}
+
+
+
+	public void setTinta(int tinta) {
+		this.tinta = tinta;
+	}
+	
 }
