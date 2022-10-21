86a615e07c2c0724b3891f817063deb3fffcd8d0
ligia <ligia@DESKTOP-33DDFM8>
2022-10-20 22:55:22 -0300

    ex cubo

---
+++ cubo/src/cubo/Cubo.java
@@ -0,0 +1,7 @@
+package cubo;
+
+public class Cubo {
+
+	
+
+}
---
+++ cubo/src/cubo/Main.java
@@ -0,0 +1,8 @@
+package cubo;
+
+public class Main {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+}
